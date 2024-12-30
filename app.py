7feb3e25f43fd0ba4e3decbadd32a9e0307b7999
Tanuj Jain <76273516+TANUJ0751@users.noreply.github.com>
2024-12-30 23:39:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,29 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 from PIL import Image
-
+import tensorflow as tf
+import gdown
 st.write("""
 # Pneumonia Predictor
 The training of the model and development of this app is done by Tanuj Jain.
 \nThis model is trained on VGG19 model using the Chest X ray dataset.
 
 """)
+file_id ="1-HQyY4P519xvcTGv-IskvNSQVmL9NkHs"
+url = f'https://drive.google.com/uc?export=download&id={file_id}'
+def load_model_from_drive(url):
+    # Download the model from Google Drive using gdown
+    output_path = './model.keras'  # Temporary path to store the model file
+    gdown.download(url, output_path, quiet=False)
 
+    # Load the model from the file
+    model = tf.keras.models.load_model(output_path)
+    return model
+try:
+    model = load_model_from_drive(url)
+    st.success("Model loaded successfully!")
+except Exception as e:
+    st.error(f"Error loading model: {e}")
 # Load the image
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 if uploaded_file is not None:
@@ -22,7 +37,7 @@ if uploaded_file is not None:
     image_array=np.expand_dims(image_array,axis=0)
     image_array /= 255.0 #normailze pixel values
     # Load the model
-    model = load_model('vgg19_trained_model.keras')
+    
     # Make a prediction
     
     prediction = model.predict(image_array)
