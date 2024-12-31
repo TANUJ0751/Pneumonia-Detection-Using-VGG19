fb6fabc49ff48387456cbedbda15bc39551cc80b
Tanuj Jain <76273516+TANUJ0751@users.noreply.github.com>
2024-12-31 09:12:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 if uploaded_file is not None:
     # Convert the image to an array
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     image = load_img(uploaded_file, target_size=(224, 224))
     image_array = img_to_array(image)
     image_array=np.expand_dims(image_array,axis=0)
